cfda0b9da2950f4782c61491c6be67af29e58c45
priti kumari <151085priti@gmai;.com>
2022-02-03 14:03:53 +0800

    with two arguments with return type

--- src/main/java/com/priti/java8features/CalculatorImpl.java
+++ src/main/java/com/priti/java8features/CalculatorImpl.java
@@ -2,8 +2,8 @@ package com.priti.java8features;
 
 interface Calculator {
 //	void switchOn();
-	void sum(int input);
-
+//	void sum(int input);
+	int substract(int i1, int i2);
 }
 
 // no need to write implements here
@@ -24,8 +24,17 @@ public class CalculatorImpl {
 		 * 
 		 * calculator.switchOn();
 		 */
-		Calculator calculator = (input) -> System.out.println("Sum : " + input);
-	
-		calculator.sum(4);
+		/*
+		 * Calculator calculator = (input) -> System.out.println("Sum : " + input);
+		 * 
+		 * calculator.sum(4);
+		 */
+
+		/*
+		 * Calculator calculator = (i1, i2) ->{ return i2-i1; };
+		 */
+		Calculator calculator = (i1, i2) -> i2 - i1;
+
+		System.out.println(calculator.substract(5, 18));
 	}
 }
