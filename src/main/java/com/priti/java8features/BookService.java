5c0ac0dce2a7b1c609649201cc1dd5bf0fb393b9
priti kumari <151085priti@gmai;.com>
2022-02-03 22:29:15 +0800

    By Lambda function

--- src/main/java/com/priti/java8features/BookService.java
+++ src/main/java/com/priti/java8features/BookService.java
@@ -6,22 +6,25 @@ import java.util.List;
 
 public class BookService {
 
-	
-	// 2.approach anonymous implementation of comparator  -sort book by name in ascending order
+	// 2.approach anonymous implementation of comparator -sort book by name in
+	// ascending order
 	public List<Book> getBooksinSort() {
 		List<Book> books = new BookDao().getBooks();
-		Collections.sort(books, new Comparator<Book>() {
-
-			@Override
-			public int compare(Book o1, Book o2) {
-				return o2.getName().compareTo(o1.getName());
-				
-			}
-		});
+		/*
+		 * Collections.sort(books, new Comparator<Book>() {
+		 * 
+		 * @Override public int compare(Book o1, Book o2) { return
+		 * o2.getName().compareTo(o1.getName());
+		 * 
+		 * } });
+		 */
+		Collections.sort(books, (o1, o2) -> 
+		o2.getName().compareTo(o1.getName()));
 		return books;
 	}
+
 	public static void main(String[] args) {
-		System.out.println( new BookService().getBooksinSort()); 
+		System.out.println(new BookService().getBooksinSort());
 	}
 
 }
