8cefb632e712a73bbf61a393a2e8896a789028ca
priti kumari <151085priti@gmai;.com>
2022-02-04 11:24:07 +0800

    consumer method in lambda fun

--- src/main/java/com/priti/pre/functional/demo/ConsumerDemo.java
+++ src/main/java/com/priti/pre/functional/demo/ConsumerDemo.java
@@ -2,17 +2,21 @@ package com.priti.pre.functional.demo;
 
 import java.util.function.Consumer;
 
-public class ConsumerDemo implements Consumer<Integer> {
+public class ConsumerDemo {
 
-	@Override
-	public void accept(Integer t) {
-		System.out.println("Printing : "+t);
-		
-	}
+	/*
+	 * @Override public void accept(Integer t) {
+	 * System.out.println("Printing : "+t);
+	 * 
+	 * }
+	 */
 	public static void main(String[] args) {
-		ConsumerDemo c1 = new ConsumerDemo();
-		c1.accept(12);
-		
+		/*
+		 * ConsumerDemo c1 = new ConsumerDemo(); c1.accept(12);
+		 */
+		Consumer<Integer> c = t -> System.out.println("Printing : " + t);
+
+		c.accept(10);
 	}
 
 }
